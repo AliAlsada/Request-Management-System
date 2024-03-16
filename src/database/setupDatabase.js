4fa77f04a98a1f9ee633b34407790d3887be5c86
AliAlsada <ali-alsadah1941@hotmail.com>
2024-03-16 21:41:00 +0300

    typo

--- src/database/setupDatabase.js
+++ src/database/setupDatabase.js
@@ -16,7 +16,7 @@ export async function setupDatabase() {
         `CREATE TABLE IF NOT EXISTS NewLicense (
             LicenseRequestID INTEGER PRIMARY KEY AUTOINCREMENT,
             RequestID INTEGER,
-            licenceType TEXT,
+            LicenceType TEXT,
             IsOffice BOOLEAN,
             OfficeName TEXT,
             OfficeServiceNumber TEXT,
